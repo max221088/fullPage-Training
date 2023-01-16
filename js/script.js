99c38b7ba323d03a417d39db48c24afc6913f7bb
max221088 <max.work221088@gmail.com>
2023-01-16 23:57:46 +0200

    add menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 var myFullpage = new fullpage("#fullpage", {
 	sectionsColor: ["#f2f2f2", "#4BBFC3", "#7BAABE", "whitesmoke", "#ccddff"],
+	anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage", "lastPage"],
+	menu: "#header-menu",
+	scrollBar: false,
+	controlArrows: true,
+	loopBottom: true,
+	navigation: true,
+	navigationPosition: "left",
 });
